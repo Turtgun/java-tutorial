de163d9c8268459959f9ea993207f61e8210fe11
Turtgun <icisraelcarrillo@gmail.com>
2021-09-27 00:12:19 +0000

    Upload New File

---
+++ epic.sh
@@ -0,0 +1,10 @@
+#/usr/bin/bash
+
+javac epic/Main.java
+javac epic/Dog.java
+javac epic/Cat.java
+javac epic/Fish.java
+javac epic/Goldfish.java
+javac epic/Teacher.java
+javac epic/Level.java
+java epic.Main
